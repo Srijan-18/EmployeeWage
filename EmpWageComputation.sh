ba4e9be8f7680986dbb160093511c5f99f6d6682
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-01 15:36:12 +0530

    Attendace Code Updated

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -3,7 +3,7 @@
 echo "Welcome to Employee Wage Computation"
 empAttendace=$(( RANDOM%2 ))
 IS_PRESENT=1
-if [ $empAttendance -eq 1 ]
+if [ $empAttendance -eq $IS_PRESENT ]
 then
 echo "Employee is Present"
 else
