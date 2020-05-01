4c10943c3e44420598094ee1498b6b20699f662a
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-01 15:39:48 +0530

    Error Handelled

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,12 +1,12 @@
-#!/bash/bin -x
+#!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation"
 empAttendace=$(( RANDOM%2 ))
 IS_PRESENT=1
 if [ $empAttendance -eq $IS_PRESENT ]
 then
-echo "Employee is Present"
+	echo "Employee is Present"
 else
-echo "Employee is Absent"
+	echo "Employee is Absent"
 fi
 
