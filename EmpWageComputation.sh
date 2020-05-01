24dd35378ea060f7509a6f0bc89ec36bec8017bb
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-01 18:26:24 +0530

    Monthly Wage Calculated UC5

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,23 +4,30 @@ echo "Welcome to Employee Wage Computation"
 
 IS_FULL_TIME=1
 IS_PART_TIME=2
+MAX_WORKING_DAYS=20
 WAGE_PER_HOUR=20
-employeeDailyWage=0;
+employeeDailyWage=0
 employeeWorkHours=0
-employeeAttendance=$(( RANDOM%3 ))
-case $employeeAttendance in
-	$IS_PART_TIME)
-		employeeWorkHours=8
-	;;
 
-	$IS_FULL_TIME)
-		employeeWorkHours=8
-	;;
+employeeMonthlyWage=0;
+for (( count=1;count<=$MAX_WORKING_DAYS; count++))
+do
+	employeeAttendance=$(( RANDOM%3 ))
+	case $employeeAttendance in
+		$IS_PART_TIME)
+			employeeWorkHours=8
+		;;
 
-	*)
-		employeeWorkHours=0
-	;;
-esac
+		$IS_FULL_TIME)
+			employeeWorkHours=8
+		;;
 
-employeeDailyWage=$(( WAGE_PER_HOUR*employeeWorkHours))
+		*)
+			employeeWorkHours=0
+		;;
+	esac
+
+	employeeDailyWage=$((WAGE_PER_HOUR*employeeWorkHours))
+	employeeMonthlyWage=$((employeeMonthlyWage+employeeDailyWage))
+done
 
