53f359f24209c5a8b0b62a0f8092935bb5d1fbea
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-01 18:44:35 +0530

    Calculations Based on both Monthly Hous and Days done UC6

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -5,29 +5,33 @@ echo "Welcome to Employee Wage Computation"
 IS_FULL_TIME=1
 IS_PART_TIME=2
 MAX_WORKING_DAYS=20
+MAX_WORKING_HOURS=100
 WAGE_PER_HOUR=20
 employeeDailyWage=0
+employeeDailyHours=0
 employeeWorkHours=0
-
+employeeWorkDays=0
 employeeMonthlyWage=0;
-for (( count=1;count<=$MAX_WORKING_DAYS; count++))
+while  (( employeeWorkHours<MAX_WORKING_HOURS && employeeWorkDays<MAX_WORKING_DAYS ))
 do
 	employeeAttendance=$(( RANDOM%3 ))
 	case $employeeAttendance in
 		$IS_PART_TIME)
-			employeeWorkHours=8
+			employeeDailyHours=8
+			employeeWorkHours=$((employeeWorkHours+employeeDailyHours))
 		;;
 
 		$IS_FULL_TIME)
-			employeeWorkHours=8
+			employeeDailyHours=8
+			employeeWorkHours=$((employeeWorkHours+employeeDailyHours))
 		;;
 
 		*)
-			employeeWorkHours=0
+			employeeDailyHours=0
 		;;
 	esac
-
-	employeeDailyWage=$((WAGE_PER_HOUR*employeeWorkHours))
+	((employeeWorkDays++))
+	employeeDailyWage=$((WAGE_PER_HOUR*employeeDailyHours))
 	employeeMonthlyWage=$((employeeMonthlyWage+employeeDailyWage))
 done
 
