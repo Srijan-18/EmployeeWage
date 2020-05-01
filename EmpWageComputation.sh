07dc96189fb7e1600f791c8a68ee4afb983867b2
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-01 15:25:20 +0530

    Employe Attendace Taken UC1

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,4 +1,12 @@
 #!/bash/bin -x
 
 echo "Welcome to Employee Wage Computation"
+empAttendace=$(( RANDOM%2 ))
+IS_PRESENT=1
+if [ $empAttendance -eq 1 ]
+then
+echo "Employee is Present"
+else
+echo "Employee is Absent"
+fi
 
